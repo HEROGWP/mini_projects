fa5f2c847c2de488da5cd062757212eb06595632
HEROGWP <pp820819@gmail.com>
2016-10-02 18:47:32 +0800

    topics_index has to authenticate_user

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,5 @@
 class TopicsController < ApplicationController
-	before_action :authenticate_user!, :except => [:index]
+	before_action :authenticate_user!
 	before_action :topics, :only => [:index, :create, :update]
 	before_action :topic, :only => [:show, :update, :destroy]
 	
