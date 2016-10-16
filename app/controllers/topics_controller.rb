60c356aa67ce0c79e497db752659a6fbdd9c1153
HEROGWP <pp820819@gmail.com>
2016-10-17 01:06:32 +0800

    topic show facebook og set

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -43,7 +43,7 @@ class TopicsController < ApplicationController
 		else
 			@comments = @comments.where(:status => "published")
 		end
-
+		render layout: "topic_show"
 	end
 	def create
 		@topic = current_user.topics.build(topic_params)
