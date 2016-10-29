03e7d993824ffcb2a5cab43a6a7b9a020f6b8119
HEROGWP <pp820819@gmail.com>
2016-10-30 01:58:56 +0800

    update videos short_url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,8 @@ module ApplicationHelper
 	def og_image_url(image_path)
 		request.protocol + request.host_with_port + image_path
 	end
+
+	def set_video_url(path)
+		request.protocol + request.host_with_port + '/' + path
+	end
 end
