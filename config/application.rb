45813080aa14975700b5d0c61a19b220bd5131a9
HEROGWP <pp820819@gmail.com>
2016-10-29 22:27:01 +0800

    set time_zone taipei

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,6 @@ module MiniProjects
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = "Taipei"
   end
 end
